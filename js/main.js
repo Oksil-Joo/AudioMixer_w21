499859f8bedc222c63be4b027a212d8d97fd8f5d
Oksil-Joo <jueun1125!>
2021-02-24 14:45:51 -0500

    updatingAll files

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
   console.log('fired!');
 //variabler at the top
   let theAudio = document.querySelector("audio"),
-      playButton = document.querySelector("button"),
+      controlButtons = document.querySelectorAll(".audio-controls button"),
       albumArt = document.querySelectorAll(".track-ref");
 //functions go here
-  function playTrack() {
+  function loadTrack() {
     //debugger;
     //grap the value of the custom data attribute
     let targetTrack = this.dataset.trackref;
@@ -17,12 +17,30 @@
     theAudio.volume = 0.5;
     // load and play the new source
     theAudio.load();
-    theAudio.play();
+    playTrack();
 
   }
 
+  function rewindTrack() {
+    //rewind the current track back to the beginning
+    theAudio.currentTime = 0;
+  }
+  function stopTrack() {
+    //roud breackets is a method -> something the element can do
+    theAudio.pause();
+  }
+
+  function playTrack() {
+    //run the play method ()
+    theAudio.play();
+  }
 //event handling at the bottom
 for (cover of albumArt) {
-  cover.addEventListener("click", playTrack);
+  cover.addEventListener("click", loadTrack);
 }
+
+// get the play putton -> the first element in the collection
+controlButtons[0].addEventListener("click", playTrack);
+controlButtons[1].addEventListener("click", stopTrack);
+controlButtons[2].addEventListener("click", rewindTrack);
 })()

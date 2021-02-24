f6e94cc486158f0c6627f12a031e89cb372a9346
Oksil-Joo <jueun1125!>
2021-02-24 14:25:23 -0500

    added dynamic audio

--- js/main.js
+++ js/main.js
@@ -2,4 +2,27 @@
 (() => {
   // select and work with our audio element
   console.log('fired!');
+//variabler at the top
+  let theAudio = document.querySelector("audio"),
+      playButton = document.querySelector("button"),
+      albumArt = document.querySelectorAll(".track-ref");
+//functions go here
+  function playTrack() {
+    //debugger;
+    //grap the value of the custom data attribute
+    let targetTrack = this.dataset.trackref;
+
+    //load the new audio source
+    theAudio.src = `audio/${targetTrack}.mp3`;
+    theAudio.volume = 0.5;
+    // load and play the new source
+    theAudio.load();
+    theAudio.play();
+
+  }
+
+//event handling at the bottom
+for (cover of albumArt) {
+  cover.addEventListener("click", playTrack);
+}
 })()
